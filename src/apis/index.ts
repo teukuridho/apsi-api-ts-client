4e28024e71db812185a39977c8a63705cc38b253
Teuku Ridho - Witech <teukuridho@witech.co.id>
2025-04-29 15:06:25 +0700

    feat: change generator to openapi

---
+++ src/apis/index.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AttendanceApi';
+export * from './AuthenticationApi';
+export * from './BookApi';
+export * from './BookCategoriesApi';
+export * from './ClassRoomApi';
+export * from './RootApi';
+export * from './ScheduleApi';
+export * from './StudyMaterialApi';
+export * from './SubjectApi';
